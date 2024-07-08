be8a40dbdbc808b7113acb1e2d10e01499af8223
Dhaval Sojitra <dhavalsojitra875@gmail.com>
2024-07-08 21:55:50 +0530

    dsfs

---
+++ Exercise/src/main/kotlin/def1.kt
@@ -0,0 +1,6 @@
+class def1 {
+}//1. Write a program that prints your name and your college name.
+fun main() {
+    println("Dhaval Sojita")
+    println("Atmiya University")
+}
---
+++ Exercise/src/main/kotlin/def2.kt
@@ -0,0 +1,5 @@
+//2. Write a program that prints your address with name
+fun main() {
+    println("Dhaval H. Sojitra")
+    println("To.Rasnal,Plot Vistar\nTa.Gadhada,Dist.Botad\nPin.364730")
+}
